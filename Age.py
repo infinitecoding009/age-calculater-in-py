1dc7305a9a1b5fdfd380ed768fa2a7708a765a73
55 Coder Tech <90313856+Codertech-550@users.noreply.github.com>
2021-09-08 19:28:16 +0530

    Create Age.py

---
+++ Age.py
@@ -0,0 +1,10 @@
+user = int(input())
+
+count = 0
+for user in range(user, 2021):
+    count += 1
+if count >= 1:
+# Returns how old you are.
+    print("You are/almost %d years old. Keep Gracing😀" % (count))
+else:
+    print("You have not been born 😅. Pray hard.😂")
